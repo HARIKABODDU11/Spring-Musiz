ba637f75228b16cf53b6c3e9151b38352b458b0b
HARIKABODDU11 <51943015+HARIKABODDU11@users.noreply.github.com>
2019-08-07 22:11:33 +0530

    Update UserRepository.java

--- src/main/java/com/stackroute/repository/UserRepository.java
+++ src/main/java/com/stackroute/repository/UserRepository.java
@@ -4,7 +4,7 @@ import com.stackroute.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.RequestParam;
-
+//Repository which extends the Jpa Repository
 @Repository
 public interface UserRepository extends JpaRepository<User,Integer> {
 }

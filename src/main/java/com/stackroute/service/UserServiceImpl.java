0f5b81693958c47348e89e13c20919cb64ec4405
HARIKABODDU11 <51943015+HARIKABODDU11@users.noreply.github.com>
2019-08-07 21:58:37 +0530

    Update UserServiceImpl.java

--- src/main/java/com/stackroute/service/UserServiceImpl.java
+++ src/main/java/com/stackroute/service/UserServiceImpl.java
@@ -10,26 +10,29 @@ import java.util.List;
 @Service
 
 public class UserServiceImpl implements UserService {
-
-    UserRepository userRepository;
-
+    //Autowiring the muzix repository
     @Autowired
+    UserRepository userRepository;
+    
+    // parameter constructor
+   
     public UserServiceImpl(UserRepository userRepository){
         this.userRepository=userRepository;
 
     }
+    //save the user data
     @Override
     public User saveUser(User user) {
         User saveUser=userRepository.save(user);
         return saveUser;
     }
-
+  //retrieve the users data
     @Override
     public List<User> getAllUsers() {
         return userRepository.findAll();
     }
     
-    
+    //Updating the existing user
     @Override
     public User updateUser(User user, int id) throws UserNotFoundException
         {
@@ -47,27 +50,10 @@ public class UserServiceImpl implements UserService {
             return savedUser;
         }
 
+     //Deleting the user
     @Override
-
-    public boolean deleteUser(int id) throws UserNotFoundException
-    {
-        Optional<User> user1 = userRepository.findById(id);
-
-        if(!user1.isPresent())
-        {
-            throw new UserNotFoundException("Track Not Found");
-        }
-
-        try {
-
-            userRepository.delete(user1.get());
-
-            return true;
-
-        }
-        catch (Exception exception)
-        {
-            return false;
-        }
+    public User deleteUser(int id) {
+        return userRepository.deleteById(id);
     }
+
 }

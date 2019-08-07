2fa0eb1e8ae15c889062d1722e45927a5999e3bc
HARIKABODDU11 <51943015+HARIKABODDU11@users.noreply.github.com>
2019-08-07 23:58:31 +0530

    Update UserService.java

--- src/main/java/com/stackroute/service/UserService.java
+++ src/main/java/com/stackroute/service/UserService.java
@@ -5,14 +5,14 @@ import com.stackroute.exceptions.UserAlreadyExistsException;
 import com.stackroute.exceptions.UserNotFoundException;
 
 import java.util.List;
-
+//Service interface
 public interface UserService {
-
+//methods in the interface
     public User saveUser(User user) throws UserAlreadyExistsException;
     public List<User> getAllUsers();
     public List<User> userByName(String name) throws UserNotFoundException;
 
     public User updateUser(User user, int id) throws UserAlreadyExistsException;
 
-    public boolean deleteUser(int id) throws UserNotFoundException;
+    public User deleteUser(int id) throws UserNotFoundException;
 }

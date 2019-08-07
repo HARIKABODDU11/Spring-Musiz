1cffda6784a4d9f31684c45923f7a8720adf6e0a
HARIKABODDU11 <51943015+HARIKABODDU11@users.noreply.github.com>
2019-08-07 22:13:34 +0530

    Update UserService.java

--- src/main/java/com/stackroute/service/UserService.java
+++ src/main/java/com/stackroute/service/UserService.java
@@ -3,9 +3,13 @@ package com.stackroute.service;
 import com.stackroute.domain.User;
 
 import java.util.List;
-
+//Service interface
 public interface UserService {
-
+//methods in the interface
     public User saveUser(User user);
     public List<User> getAllUsers();
+    
+    public User deleteUser(int id);
+
+    public User updateUser(User user);
 }

9d9c93ad9b12af9839dc536a5a83e8e9e7d93530
HARIKABODDU11 <51943015+HARIKABODDU11@users.noreply.github.com>
2019-08-07 22:58:33 +0530

    Update UserServiceImpl.java

--- src/main/java/com/stackroute/service/UserServiceImpl.java
+++ src/main/java/com/stackroute/service/UserServiceImpl.java
@@ -10,30 +10,63 @@ import java.util.List;
 @Service
 
 public class UserServiceImpl implements UserService {
-
+     //Autowiring the muzix repository
+  @Autowired
     UserRepository userRepository;
 
-    @Autowired
+  //constructor
     public UserServiceImpl(UserRepository userRepository){
         this.userRepository=userRepository;
 
     }
+    //save the user
     @Override
     public User saveUser(User user) {
         User saveUser=userRepository.save(user);
         return saveUser;
     }
-
+//get data of all users
     @Override
     public List<User> getAllUsers() {
         return userRepository.findAll();
     }
-
+    
+    
+    
+     //Deleting the user
     @Override
-    public List<User> userByName(String name) {
+    public boolean deleteUser(int id) {
+        userRepository.deleteById(id);
+        return true;
+    }
+      //Tracking the data by userbyname
+    @Override
+    public List<User> userByName(String name)
+    {
 
         return userRepository.userByName(name);
     }
+
+
+    //Updating the existing user
+    @Override
+    public User updateUser(User user) {
+        Optional<User> user1 = userRepository.findById(user.getId());
+
+        if (user1.isPresent()) {
+            User newEntity = user1.get();
+            newEntity.setId(muzix.getId());
+            newEntity.setName(muzix.getName());
+            newEntity.setTrack(muzix.getTrack());
+            newEntity = userRepository.save(newEntity);
+            return newEntity;
+        } else {
+            user = userRepository.save(user);
+
+            return user;
+        }
+    }
+
 
 
 }

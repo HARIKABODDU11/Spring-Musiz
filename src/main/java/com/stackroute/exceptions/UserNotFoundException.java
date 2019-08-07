db0c98a6011d05c3fd2901dab7733ac908f881ce
HARIKABODDU11 <51943015+HARIKABODDU11@users.noreply.github.com>
2019-08-07 23:33:17 +0530

    Update UserNotFoundException.java

--- src/main/java/com/stackroute/exceptions/UserNotFoundException.java
+++ src/main/java/com/stackroute/exceptions/UserNotFoundException.java
@@ -1,8 +1,8 @@
 package com.stackroute.exceptions;
-
+//Custom exception
 public class UserNotFoundException extends Exception {
     private String message;
-
+//Default and paramterized constructor
     public UserNotFoundException() {}
     public UserNotFoundException(String message){
         super(message);

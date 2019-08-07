5e1f18f3cc46d0ea890e1826f8994fd639d4137f
HARIKABODDU11 <51943015+HARIKABODDU11@users.noreply.github.com>
2019-08-07 23:53:06 +0530

    Update UserController.java

--- src/main/java/com/stackroute/controller/UserController.java
+++ src/main/java/com/stackroute/controller/UserController.java
@@ -13,26 +13,28 @@ import java.util.List;
 
 @RestController
 @RequestMapping(value = "api/v1")
+//Controller class
 public class UserController {
+    //Creating object for the muzix service
     UserService userService;
-
+//parameterized constructor
     public UserController(UserService userService) {
         this.userService = userService;
     }
-
+//save the user
     @PostMapping("user")
     public ResponseEntity<?> saveUser(@RequestBody User user) throws UserAlreadyExistsException {
             userService.saveUser(user);
             return new ResponseEntity<String>("Successfully created", HttpStatus.CREATED);
     }
-
+// get all users
     @GetMapping("user")
     public ResponseEntity<?> getAllUsers() {
         userService.getAllUsers();
         return new ResponseEntity<List<User>>(userService.getAllUsers(), HttpStatus.OK);
     }
 
-
+//search the user by name
     @GetMapping("userByName")
     public ResponseEntity<?> userByName(@RequestParam String name) throws UserNotFoundException{
         userService.userByName(name);
@@ -41,13 +43,13 @@ public class UserController {
 
 
 
-
+//uodate the user
     @PutMapping("user/{id}")
     public ResponseEntity<?> updateUser(@RequestBody User user, @PathVariable int id) throws   UserAlreadyExistsException {
         userService.updateUser(user, id);
         return new ResponseEntity<List<User>>(userService.getAllUsers(), HttpStatus.CREATED);
     }
-
+ //Deleting the user
     @DeleteMapping("user/{id}")
     public ResponseEntity<?> deleteUser(@PathVariable int id) throws UserNotFoundException {
         userService.deleteUser(id);

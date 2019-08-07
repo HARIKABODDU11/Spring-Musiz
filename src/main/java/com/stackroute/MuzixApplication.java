0a12fdc3198f05729f369e9f5739b9f6ea6724fa
HARIKABODDU11 <51943015+HARIKABODDU11@users.noreply.github.com>
2019-08-07 22:21:27 +0530

    Update MuzixApplication.java

--- src/main/java/com/stackroute/MuzixApplication.java
+++ src/main/java/com/stackroute/MuzixApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+//Run method which is used to run the application
 public class MuzixApplication {
 
 	public static void main(String[] args) {

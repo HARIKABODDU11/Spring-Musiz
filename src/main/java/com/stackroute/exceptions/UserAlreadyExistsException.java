f73a30922efb5ad3847236cd7707b70f34105754
HARIKABODDU11 <51943015+HARIKABODDU11@users.noreply.github.com>
2019-08-08 09:56:30 +0530

    Update UserAlreadyExistsException.java

--- src/main/java/com/stackroute/exceptions/UserAlreadyExistsException.java
+++ src/main/java/com/stackroute/exceptions/UserAlreadyExistsException.java
@@ -2,7 +2,7 @@ package com.stackroute.exceptions;
 
 public class UserAlreadyExistsException extends Exception {
     private String message;
-
+   //parametrized and non parametrized constructor
     public UserAlreadyExistsException() {}
     public UserAlreadyExistsException(String message){
         super(message);
